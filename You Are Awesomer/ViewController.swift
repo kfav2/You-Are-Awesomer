3e7e3481492bdc1c558d7b7625cec0701241cc83
KFAV <kofava14@gmail.com>
2021-12-24 20:33:40 -0800

    Initial Commit

---
+++ You Are Awesomer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You Are Awesomer
+//
+//  Created by Korlin Favara on 12/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
